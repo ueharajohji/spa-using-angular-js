32202a71448e3e4272eecabe78b5a4356ec3b2e6
Uehara <test.gmail.com>
2017-09-17 11:33:52 +0900

    remove ruby api code for demo

--- recipes_api/app/models/ingredient.rb
+++
@@ -1,3 +0,0 @@
-class Ingredient < ApplicationRecord
-  belongs_to :recipe , optional: true
-end
--- recipes_api/app/models/recipe.rb
+++
@@ -1,4 +0,0 @@
-class Recipe < ApplicationRecord
-  has_many :ingredients
-  accepts_nested_attributes_for :ingredients
-end
